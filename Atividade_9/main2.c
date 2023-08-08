8f72803a7ed8f42fe2e747b2efb25fbdd14e8a56
JorgeHenriqueBorgesSantos <120414588+JorgeHenriqueBorgesSantos@users.noreply.github.com>
2023-08-08 17:27:19 -0400

    Add files via upload

---
+++ Atividade_9/main2.c
@@ -0,0 +1,41 @@
+#include "ToDoList.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    Descritor* Semana[7];
+    int h, y, t;
+
+    for (h = 0; h < 7; h++)
+    {
+        Semana[h] = criar();
+    }
+
+    h=0;
+    while(0<=h<7)
+    {
+        scanf("%d", &h);
+        if(h>7||h<0)
+        {
+            break;
+        }
+        Insert(Semana, h);
+    }
+    printf("\n");
+
+    Printar(Semana);
+
+    printf("\n");
+    
+    for (h = 0; h < 7; h++)
+    {
+        y = Semana[h]->quant;
+        for (t=0; t < y; t++)
+        {
+            Pop(Semana, h);
+        }
+    }
+
+    printf("Tarefas da semana concluidas(Lista vazia)");
+}
