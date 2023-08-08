ce9a98ab9bf4320ea3c4506f897b3a9619abf2ec
nezi404 <105327627+nezi404@users.noreply.github.com>
2023-08-08 19:31:55 -0400

    Add files via upload
    esse foi o primeiro q fiz, acho q ele só consegue ir pelos zeros e esquivar do 1. to arrumando o outro pq não to conseguindo desempilhar mas ctz que é pq eu fiz a função errada kk

---
+++ pilha.h
@@ -0,0 +1,189 @@
+typedef struct pilha Pilha;
+
+struct pilha
+{
+    Pilha* prox;
+    int i;
+    int j;
+};
+
+Pilha* criar_pilha()
+{   
+    return NULL;
+}
+
+Pilha* adicionar_pilha(int i, int j, Pilha *p)
+{
+    Pilha *p_temp;
+    p_temp= (Pilha *)malloc(sizeof(Pilha));
+    p_temp->prox=p;
+    p_temp->i=i;
+    p_temp->j=j;
+
+    return p_temp;   
+}
+
+Pilha* remover_pilha(Pilha *p)
+{
+    Pilha *p_temp;
+    p_temp= (Pilha *)malloc(sizeof(Pilha));
+    p_temp=p;
+    p=p_temp->prox;
+    p_temp=NULL;
+    free(p_temp);
+    
+    return p;
+}
+
+void ler_arquivo(int array_inteiro[][10], int n)
+{
+    int i, j, lin=n, col=n;
+    char array[lin][col][2];
+    char verif[2];
+
+    for(i=0;i<lin;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            fgets(verif, 2, stdin);
+
+                if (strcmp(verif,"\n\0")!=0 && strcmp(verif,"\r\0")!=0)
+                {
+                    //printf("copying...\n");
+                    printf("%s\n", verif);
+                    strcpy(array[i][j], verif);
+                    array_inteiro[i][j]=atoi(array[i][j]);;
+                }
+                else
+                    j--;
+        }
+    }
+
+}
+
+
+/*
+void ler_arquivo(int array_inteiro[][10], int n)
+{
+    int i, j, lin=n, col=n;
+    char array_char[lin][col];
+    char *num = malloc(2*sizeof(char));
+
+    printf("Insert numbers: ");
+    for(i=0;i<lin;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            array_char[i][j] = getchar();
+            if(isspace(array_char[i][j]))
+                j--;
+            else
+            {
+            num[0]=array_char[i][j];
+            num[1]="\0";
+            array_inteiro[i][j]=atoi(num);
+            }
+        }
+    }
+
+}
+*/
+
+
+void print_array(int array[][10], int n)
+{
+    int i, j, lin=n, col=lin;
+    printf("Print do array\n");
+     for(i=0;i<lin;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            printf("%d\t", array[i][j]);
+        }
+        printf("\n");
+    }
+
+
+}
+
+void copiar_matriz(int mat[][10], int mat_copia[][10], int n)
+{
+    int i, j;
+
+    for (i=0; i<n; i++){
+        for(j=0; j<n; j++){
+            mat_copia[i][j]=mat[i][j];
+        }
+    }
+}
+
+/*
+0 - espaço em branco
+1 - parede
+2 - caminho percorrido
+3 - caminho correto (resposta final)
+4 - beco sem saída
+*/
+
+/*
+direita array[i][j+1]
+baixo array[i+1][j]
+esquerda array[i][j-1]
+cima array[i-1][j]
+*/
+
+void achar_caminho(int array[][10], int n, Pilha* p ){
+    int i=1, j=0, final=0;
+
+    while(final==0){
+        
+        //se direita
+         if(array[i][j+1]==0 || (i==n-2 && j+1==n-1)){
+            j++;
+            array[i][j]=2;
+            p = adicionar_pilha(i, j, p);
+            printf("Pra direita!\n");
+        }
+        //baixo
+        else{
+            if(array[i+1][j]==0 || (i==n-2 && j+1==n-1)){
+            i++;
+            array[i][j]=2;
+            p = adicionar_pilha(i, j, p);
+            printf("Pra baixo\n");
+            }
+            else{
+                //esquerda
+                if(array[i][j-1]==0)
+                {
+                    j--;
+                    array[i][j]=2;
+                    p = adicionar_pilha(i, j, p);
+                    printf("Pra esquerda!\n");
+                }
+                else
+                {   //cima
+                    if(array[i-1][j]==0){
+                        i--;
+                        array[i][j]=2;
+                        p = adicionar_pilha(i, j, p);
+                        printf("Pra cima!\n");
+                    }
+                    printf("Pra cima naum dah tambemm\n");
+                }
+
+                }
+            }
+            print_array(array, n);
+            //printf("linha %d  coluna %d\n", p->i, p->j);
+
+            if (p->i==n-2 && p->j==n-1){
+            printf("\n\n\nChegou no finalll\n\n\n");
+            final=1; 
+            }  
+        }
+        
+    }
+    
+
+
